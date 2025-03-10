1d7db952fe03a676731f3c929c3bd458f1e0f22e
indranild de <84306058+indranil07-8@users.noreply.github.com>
2025-03-10 17:42:03 +0530

    show station table on home page

--- Weather API/main.py
+++ Weather API/main.py
@@ -1,26 +1,28 @@
-from flask import Flask ,render_template
+from flask import Flask, render_template
 import pandas as pd
-app= Flask("website")
+
+app = Flask("website")
+
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    df = pd.read_csv("data_small/stations.txt", skiprows=17)
+    df= df[["STAID", "STANAME                                 "]]
+
+    return render_template("home.html", data=df.to_html())
+
 
 @app.route("/API/v1/<station>/<date>")
-def about(station,date):
+def about(station, date):
     date = f"{date[:4]}-{(date[4:6])}-{(date[6:])}"
-    filename = "data_small/TG_STAID"+str(station).zfill(6) + ".txt"
-    df = pd.read_csv(filename,skiprows=20,parse_dates=["    DATE"])
-    temperature = df.loc[df["    DATE"] == (date)]['   TG'].squeeze()/10
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    temperature = df.loc[df["    DATE"] == (date)]['   TG'].squeeze() / 10
     return {
-        "station":station,
-        "date":date,
+        "station": station,
+        "date": date,
         "temperature": temperature
     }
-
-
-
-
 
 
 app.run(debug=True)
